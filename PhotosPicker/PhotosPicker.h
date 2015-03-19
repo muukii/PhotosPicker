0f9824e4fbf51613139259bebd3b3af8946e78a9
muukii <muukii.muukii@gmail.com>
2015-03-20 01:39:55 +0900

    init commit

---
+++ PhotosPicker/PhotosPicker.h
@@ -0,0 +1,19 @@
+//
+//  PhotosPicker.h
+//  PhotosPicker
+//
+//  Created by Muukii on 3/20/15.
+//  Copyright (c) 2015 muukii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PhotosPicker.
+FOUNDATION_EXPORT double PhotosPickerVersionNumber;
+
+//! Project version string for PhotosPicker.
+FOUNDATION_EXPORT const unsigned char PhotosPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhotosPicker/PublicHeader.h>
+
+
