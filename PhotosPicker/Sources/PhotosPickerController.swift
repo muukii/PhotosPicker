38b9e00f8d7c93fe6b2ea13ce301bf9dd112a018
muukii <muukii.muukii@gmail.com>
2015-03-22 22:47:01 +0900

    Add Example, some classes

---
+++ PhotosPicker/Sources/PhotosPickerController.swift
@@ -0,0 +1,19 @@
+//
+//  PhotosPickerController.swift
+//  PhotosPicker
+//
+//  Created by Muukii on 3/22/15.
+//  Copyright (c) 2015 muukii. All rights reserved.
+//
+
+import UIKit
+
+class PhotosPickerController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
